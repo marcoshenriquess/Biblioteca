901f826bed22c2f5977e701b3f702ac35cbc5ee1
Daniela Castro <105952842+danielacastros@users.noreply.github.com>
2023-03-28 20:31:57 -0300

    classe de conexao

---
+++ utils/database.js
@@ -0,0 +1,59 @@
+var mysql = require('mysql2');
+
+class Database {
+
+    #conexao;
+
+    get conexao() { return this.#conexao;} 
+    set conexao(conexao) { this.#conexao = conexao; }
+
+    constructor() {
+
+        this.#conexao = mysql.createPool({
+            host: '140.238.185.50',
+            database: 'DB_10442211252',
+            user: '10442211252',
+            password: '10442211252',
+        });
+        
+    }
+
+    ExecutaComando(sql, valores) {
+        var cnn = this.#conexao;
+        return new Promise(function(res, rej) {
+            cnn.query(sql, valores, function (error, results, fields) {
+                if (error) 
+                    rej(error);
+                else 
+                    res(results);
+            });
+        })
+    }
+    
+    ExecutaComandoNonQuery(sql, valores) {
+        var cnn = this.#conexao;
+        return new Promise(function(res, rej) {
+            cnn.query(sql, valores, function (error, results, fields) {
+                if (error) 
+                    rej(error);
+                else 
+                    res(results.affectedRows > 0);
+            });
+        })
+    }
+
+    ExecutaComandoLastInserted(sql, valores) {
+        var cnn = this.#conexao;
+        return new Promise(function(res, rej) {
+            cnn.query(sql, valores, function (error, results, fields) {
+                if (error) 
+                    rej(error);
+                else 
+                    res(results.insertId);
+            });
+        })
+    }
+
+}
+
+module.exports = Database;
