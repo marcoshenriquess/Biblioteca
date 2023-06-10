511fddadab75701207773ecb664f856ac997549f
Victor <victorhugo.op@outlook.com>
2023-06-10 15:36:53 -0300

    Nome usuario Logado

--- routes/usuarioRouter.js
+++ routes/usuarioRouter.js
@@ -2,26 +2,26 @@ const express = require('express');
 const UserController = require('../controller/usuarioController');
 const Autenticacao = require('../middlewares/autenticacao');
 
-class UserRouter{
+class UserRouter {
     #router;
-    get router(){
+    get router() {
         return this.#router;
     }
 
-    set router(router){
+    set router(router) {
         this.#router = router;
     }
 
-    constructor(){
+    constructor() {
         this.#router = express.Router();
         let ctrl = new UserController();
         let auth = new Autenticacao();
-        this.#router.get('/',ctrl.UserView);
-        this.#router.get('/lista',ctrl.UserLista);
-        this.#router.get('/cadastro',ctrl.UserCadastroView);
+        this.#router.get('/', ctrl.UserView);
+        this.#router.get('/lista', ctrl.UserLista);
+        this.#router.get('/cadastro', ctrl.UserCadastroView);
         this.#router.get('/alterar/:id', ctrl.UserAlterarView);
-        this.#router.post('/cadastro',ctrl.UserCadastro);
-        this.#router.post('/excluir',ctrl.deletarUsuario);
+        this.#router.post('/cadastro', ctrl.UserCadastro);
+        this.#router.post('/excluir', ctrl.deletarUsuario);
         this.#router.post('/alterar', ctrl.UserAlterar);
 
     }
